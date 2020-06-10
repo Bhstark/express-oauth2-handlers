73c5b3f05be1e8eb5430d77c3a1cda52a344effb
Bhstark <bhstark@gmail.com>
2020-06-09 18:04:36 -0700

    Update routes.js

--- routes.js
+++ routes.js
@@ -33,7 +33,7 @@ exports.init = (arg1, arg2, arg3) => {
         access_type: 'offline',
         scope: scopes,
         prompt: 'consent', // Needed so we receive a refresh token every time
-        state: req.query.toString()
+        state: JSON.stringify({user_id:a})
       })
       
     );
